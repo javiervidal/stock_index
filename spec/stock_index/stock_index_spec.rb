57de9292bb16102b6916d88c37aa6303df7f360e
Javier Vidal <javier@javiervidal.net>
2014-12-04 07:47:04 +0100

    Simplifying fixture files

--- spec/stock_index/stock_index_spec.rb
+++ spec/stock_index/stock_index_spec.rb
@@ -9,18 +9,20 @@ describe StockIndex do
   # How to generate a fixture file
   #
   # require 'yaml'
-  # File.open('spec/fixtures/NDX.yaml', 'w') {|f| f.write StockIndex.new('^NDX').components.to_yaml }
+  # File.open('spec/fixtures/DJI.yaml', 'w') {|f| f.write StockIndex.new('^DJI').components.map{ |e| e[:symbol]}.to_yaml }
+  # File.open('spec/fixtures/GSPC.yaml', 'w') {|f| f.write StockIndex.new('^GSPC').components.map{ |e| e[:symbol]}.to_yaml }
+  # File.open('spec/fixtures/NDX.yaml', 'w') {|f| f.write StockIndex.new('^NDX').components.map{ |e| e[:symbol]}.to_yaml }
 
   it 'returns components for ^DJI' do
-    expect(StockIndex.new('^DJI').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^DJI').map{ |e| e[:symbol] })
+    expect(StockIndex.new('^DJI').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^DJI'))
   end
 
   it 'returns components for ^GSPC' do
-    expect(StockIndex.new('^GSPC').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^GSPC').map{ |e| e[:symbol] })
+    expect(StockIndex.new('^GSPC').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^GSPC'))
   end
 
   it 'returns components for ^NDX' do
-    expect(StockIndex.new('^NDX').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^NDX').map{ |e| e[:symbol] })
+    expect(StockIndex.new('^NDX').components.map{ |e| e[:symbol] }).to match_array(components_from_fixture('^NDX'))
   end
 
 end
