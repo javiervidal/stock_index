a92da71d1788f7118dfa7def8b420c52ff4c837c
Javier Vidal <javier@javiervidal.net>
2015-11-30 17:04:54 +0100

    Fixes #4 - NIKKEI 225 scraper has stopped working

--- lib/stock_index/scrapers/nikkei_scraper.rb
+++ lib/stock_index/scrapers/nikkei_scraper.rb
@@ -3,14 +3,14 @@ class NikkeiScraper < StockIndex::BaseScraper
   def scrape
     doc = Nokogiri::HTML(open(StockIndex::INDICES['^N225'][:url]))
     @wikipedia_hash = parse_wikipedia_page(Nokogiri::HTML(open(StockIndex::INDICES['^N225'][:wikipedia_url])))
-    parse_rows doc.css('table.cmn-table tr.cmn-charcter')
+    parse_rows doc.css('div.row.component-list')
   end
 
   private
 
   def parse_rows(rows)
     rows.inject([]) do |array, tr|
-      symbol = tr.css('td')[0].text
+      symbol = tr.css('div').text
       market = 'XJPX'
       if symbol && market
         component = StockIndex::Component.new(symbol, market, @wikipedia_hash[symbol], :jp)
